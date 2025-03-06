58de664058cd88b55bc579bc5687ea82f913759d
Thang Truong <thangtruong@Thangs-MBP.attlocal.net>
2025-03-05 23:52:36 -0600

    add more stuff

--- mirror_divide_game/platform.gd
+++ mirror_divide_game/platform.gd
@@ -1,7 +1,12 @@
 extends CharacterBody2D
 
-@onready var platform_camera = Camera2D
+@onready var platform_camera = $Camera2D
 
 func _on_lever_platform_raised() -> void:
-	
+	platform_camera.make_current()  # Activate camera transition
+	var tween = get_tree().create_tween()
+	tween.tween_property(platform_camera, "zoom", Vector2(1, 1), 2.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	await tween.finished
+	platform_camera.enabled = false  # Optionally, switch back after transition
+
 	position += Vector2(0, -210)  # Move up by 50 pixels
