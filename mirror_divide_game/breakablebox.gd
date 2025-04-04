5f0a3ac64d7abc3aeea72aa71582e0eafda25e51
Thang Truong <thangtruong@wireless-10-148-148-97.public.utexas.edu>
2025-04-04 11:45:30 -0500

    box not interact with key box and physics remain

--- mirror_divide_game/breakablebox.gd
+++ mirror_divide_game/breakablebox.gd
@@ -1,10 +1,12 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 @export var health = 1
 
 func _ready():
 	add_to_group("breakable")  # Add to breakable group
+	lock_rotation = true
 
+			
 func break_box():
 	health -= 1
 	if health <= 0:
