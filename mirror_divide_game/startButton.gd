499bed429d81aee9c629fa77cf862e8d689bf8de
Douglas Pham <phamdoug22@gmail.com>
2025-04-11 13:21:37 -0500

    title screen transition, hearts update, etc.

--- mirror_divide_game/startButton.gd
+++ mirror_divide_game/startButton.gd
@@ -1,33 +1,8 @@
 extends TextureButton
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pressed.connect(start_game)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-func _on_pressed():
-	# Preload the new scene as a PackedScene
-	#var tutorial_scene = preload("res://tutorial.tscn")
-	# Instantiate the PackedScene to get a Node
-	#var new_scene = tutorial_scene.instantiate()
-	
-	# Get the SceneTree and root node
-	#var tree = get_tree()
-	#var root = tree.root
-	
-	# Optionally, remove the current scene if it exists
-	#if tree.current_scene:
-	#	tree.current_scene.queue_free()
-	
-	# Add the new scene as a child of the root node
-	#root.add_child(new_scene)
-	
-	# Update the current_scene property
-	#tree.current_scene = new_scene
-	pass
+func start_game():
+	TransitionManager.next_scene_path = "res://tutorial.tscn"
+	TransitionManager.play_fade()
